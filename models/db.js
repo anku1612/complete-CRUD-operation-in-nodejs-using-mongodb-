a9f7f709673153a97ac7d6e3b0e8d8e97caf59fa
unknown <mallikarjunchv@gmail.com>
2019-09-26 08:33:34 +0530

    this is node js complete crud operation

---
+++ models/db.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/EmployeeDB', {useNewUrlPaerser:true},(err)=>{
+if(!err){console.log('MongoDb Connection Succeeded.')}
+else{ console.log('Error in Db Connection : ' + err)}});
+
+require('./employee.model');
